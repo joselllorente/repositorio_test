0a833a98752c80ee2086f1325f39fb1cb1e88a78
Jose Luis <joseluis.llorenteperales@gmail.com>
2022-02-21 16:41:45 +0100

    Modificación

--- src/empezandocongit/HelloWorld.java
+++ src/empezandocongit/HelloWorld.java
@@ -3,7 +3,7 @@ package empezandocongit;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo con GIT");
 
 	}
 
